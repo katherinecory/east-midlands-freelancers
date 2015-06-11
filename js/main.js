c7e85223730e206cf721219e42c65aa163b80292
Katherine Cory <hello@katherinecory.com>
2015-06-11 09:10:10 +0100

    Update the javascript to pull in the East Midlands data.

--- js/main.js
+++ js/main.js
@@ -1,56 +1,85 @@
 $(function (){
-	//load spreadsheet
-	var mySpreadsheet = 'https://docs.google.com/spreadsheet/pub?key=0AgYVWMxsntVodGlNeS13M0V1dmFmakJENTlaMWoxV2c&single=true&gid=3';
-	$('#freelancerslist').sheetrock({
-		url: mySpreadsheet,
-		//chunkSize: 0,
-		sql: 'select B,E,C,D,F,G order by A desc',
-		userCallback: function() {
-			links();
-			twitter();
-			websiteLink();
-			tableFilterApplication();
-		}
-	});
+  //load spreadsheet
+  var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1uhDr_fIByZgERFUvoqLpSsVnERhQ2xAmdwcE35WbeBw/pubhtml?gid=1469068625&single=true';
+  $('#freelancerslist').sheetrock({
+    url: mySpreadsheet,
+    chunkSize: 100,
+    sql: 'select B,C,E,F,D,G,H,I order by B asc, C asc',
+    labels: ['Name', 'Availability', 'Working', 'Location', 'Skills', 'Website URL', 'Twitter', 'Email'],
+    userCallback: function() {
+      links();
+      twitter();
+      websiteLink();
+      tableFilterApplication();
+      email();
+    }
+  });
 
-	// work out portfolio link
-	function websiteLink() {
-		$('table td a:not([href*="twitter"])').parent().addClass('website-link');
-	}
+  // work out portfolio link
+  function websiteLink() {
+    $('table td a:not([href*="twitter"])').parent().addClass('website-link');
+  }
 
-	// add a table filter
-	function tableFilterApplication() {
-		$('table').filterTable({
-			quickList: ['Branding', 'Interaction design', 'User experience', 'Front-end', 'Copywriting'],
-			placeholder: "Search this list"
-			
-	        });
-	}	
+  // add a table filter
+  function tableFilterApplication() {
+    $('#freelancerslist').filterTable({
+      quickList: [
+        'Animation',
+        'Branding',
+        'Copywriting',
+        'Drupal',
+        'Front-end',
+        'Graphic design',
+        'Illustration',
+        'Javascript',
+        'Mobile Apps',
+        '.NET',
+        'Node',
+        'Photography',
+        'PHP',
+        'Python',
+        'Ruby',
+        'UX',
+        'Videography',
+        'Wordpress'
+      ],
+      placeholder: "Search this list",
+      minRows: 1
+    });
+     // add boostrap style to table
+    $('#freelancerslist').addClass('table table-striped table-hover');
+  }
 
-	// add boostrap style to table
-	$('table').addClass('table table-striped table-hover');
+  // add boostrap style to table
+  $('table').addClass('table table-striped table-hover');
 
+  //add links
+  function links() {
+    var websiteEl = $('#freelancerslist td:nth-child(6)');
+    websiteEl.each(function(index) {
+      var link = $(this).text();
+      $(this).wrapInner('<a target="_blank" href="'+ link +'" />');
+    });
+  };
 
-	//add links
-	function links() {
-		var websiteEl = $('#freelancerslist td:nth-child(5)');
-		websiteEl.each(function(index) {
-			var link = $(this).text();
-			$(this).wrapInner('<a target="_blank" href="'+ link +'" />');
-		});	
-	};
+  //add twitter links
+  function twitter() {
+    var twitterEl = $('#freelancerslist td:nth-child(7)');
+    twitterEl.each(function(index) {
+      var link = "http://twitter.com/"; //twitter url
+      var text = $(this).text();
+      text = text.replace("@", "");
+      link += text;
+      $(this).wrapInner('<a target="_blank" href="'+ link +'" />');
+    });
+  }
 
-	//add links
-	function twitter() {
-		var twitterEl = $('#freelancerslist td:nth-child(6)');
-		twitterEl.each(function(index) {
-			var link = "http://twitter.com/"; //twitter url 
-			var text = $(this).text();
-				text = text.replace("@", "");
-			link += text;
-			$(this).wrapInner('<a target="_blank" href="'+ link +'" />');
-		});	
-	}
-
-
+  //add email links
+  function links() {
+    var emailEl = $('#freelancerslist td:nth-child(8)');
+    emailEl.each(function(index) {
+      var link = $(this).text();
+      $(this).wrapInner('<a target="_blank" href="mailto:'+ link +'" />');
+    });
+  };
 });
